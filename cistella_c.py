d8b7ebee48139652ca9e838948b5dd88dcf811bd
Fakler <62989111+Fakler@users.noreply.github.com>
2023-11-22 23:37:15 +0100

    Podries saber com comprovar si hem comprat llimones?

---
+++ cistella_c.py
@@ -0,0 +1,14 @@
+#Pomes: 3,56 €
+#Mandarines: 4,35 €
+#Síndria: 6,23 €
+#Maduixes: 4,28 €
+#Peres: 2,86
+#Taronges: 3,48
+#Dictionaries
+"""
+Podries saber com comprovar si hem comprat llimones?
+"""
+fruites_dict = dict(Pomes= 3.56, Mandarines=4.35, Síndria=6.23, Maduixes=4.28, Peres=2.86, Taronges=3.48)
+fruites = fruites_dict.keys()
+fruites_llista = list(fruites)
+print('Llimones' in fruites_llista)
